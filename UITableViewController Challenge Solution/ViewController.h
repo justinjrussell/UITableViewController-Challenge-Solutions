bbedd8b2f8cd0d9d1b71122b3abe3cd2dcadf359
Justin Russell <justinrussell@Justins-iMac.local>
2014-12-23 17:40:12 -0800

    Initial Commit

---
+++ UITableViewController Challenge Solution/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Justin Russell on 12/23/14.
+//  Copyright (c) 2014 Justin Russell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
