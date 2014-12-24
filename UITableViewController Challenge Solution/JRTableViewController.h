9813f110456ce0d174c04f58c297dfeb812b2f9e
Justin Russell <justinrussell@Justins-iMac.local>
2014-12-23 18:17:59 -0800

    Solved the challenge

--- UITableViewController Challenge Solution/JRTableViewController.h
+++ UITableViewController Challenge Solution/JRTableViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  JRTableViewController.h
 //  UITableViewController Challenge Solution
 //
 //  Created by Justin Russell on 12/23/14.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface JRTableViewController : UITableViewController
 
 @end
-
